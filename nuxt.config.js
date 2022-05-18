c4f91bde9ac069ce3e91d0c995042bac5f6e5b80
meru28 <merurw@gmail.com>
2022-05-18 10:07:03 +0700

    composition api module

--- nuxt.config.js
+++ nuxt.config.js
@@ -32,6 +32,7 @@ export default {
     '@nuxt/typescript-build',
     // https://go.nuxtjs.dev/tailwindcss
     '@nuxtjs/tailwindcss',
+    '@nuxtjs/composition-api/module'
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
