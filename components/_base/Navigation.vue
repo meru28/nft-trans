94c5b65afd9c4d52bf5aa4b43c03bbe81ff2d446
meru rendy wirawan <meru.r.wirawan@alfadigital.id>
2022-05-20 21:02:53 +0700

    tailwind config

---
+++ components/_base/Navigation.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    base button
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent } from '@nuxtjs/composition-api'
+export default defineComponent({
+  name: 'BaseButton',
+  setup () {
+    return {}
+  }
+})
+</script>
--- nuxt.config.js
+++ nuxt.config.js
@@ -18,7 +18,10 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [],
+  css: [
+    '~/assets/css/main.css',
+    '~/assets/css/fontello-d1e9bfd3/css/fontello.css'
+  ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
