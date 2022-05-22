29adc38962de62b352f5540d0ec0726f37f3cf88
CodeVectorizer <lukmanafandi10@gmail.com>
2022-05-22 22:24:54 +0700

    add sponsor slider

---
+++ components/Slider.vue
@@ -0,0 +1,66 @@
+<template>
+  <VueSlickCarousel :arrows="true" :slidesToShow="4">
+    <div class="px-4">
+      <div class="h-28 flex flex-col justify-end">
+        <img
+          src="~/assets/images/TechnologyNFT.png"
+          class="mb-2 mx-auto mix-blend-multiply"
+          alt=""
+        />
+      </div>
+      <p class="font-bold text-black">Technology NFT</p>
+    </div>
+    <div class="px-4">
+      <div class="h-28 flex flex-col justify-end">
+        <img
+          src="~/assets/images/NFTAgency.png"
+          class="mb-2 mx-auto mix-blend-multiply"
+          alt=""
+        />
+      </div>
+      <p class="font-bold text-black">NFT Agency</p>
+    </div>
+    <div class="px-4">
+      <div class="h-28 flex flex-col justify-end">
+        <img
+          src="~/assets/images/NFTYHoldings.png"
+          class="mb-2 mx-auto mix-blend-multiply"
+          alt=""
+        />
+      </div>
+      <p class="font-bold text-black">NFTY Holdings</p>
+    </div>
+    <div class="px-4">
+      <div class="h-28 flex flex-col justify-end">
+        <img
+          src="~/assets/images/HonestNFT.png"
+          class="mb-2 mx-auto mix-blend-multiply"
+          alt=""
+        />
+      </div>
+      <p class="font-bold text-black">Honest NFT</p>
+    </div>
+    <div class="px-4">
+      <div class="h-28 flex flex-col justify-end">
+        <img
+          src="~/assets/images/EntourageGlobal.png"
+          class="mb-2 mx-auto mix-blend-multiply"
+          alt=""
+        />
+      </div>
+      <p class="font-bold text-black">Entourage Global</p>
+    </div>
+  </VueSlickCarousel>
+</template>
+ 
+<script>
+import VueSlickCarousel from 'vue-slick-carousel'
+import 'vue-slick-carousel/dist/vue-slick-carousel.css'
+// optional style for arrows & dots
+import 'vue-slick-carousel/dist/vue-slick-carousel-theme.css'
+
+export default {
+  name: 'MyComponent',
+  components: { VueSlickCarousel },
+}
+</script> 
--- pages/index.vue
+++ pages/index.vue
@@ -2,9 +2,21 @@
   <div>
     <!-- Section 2 - About -->
     <section class="">
-      <div class="max-w-7xl mx-auto sm:px-15 md:px-10 lg:px-15 xl:px-0 py-20 lg:flex lg:justify-around gap-x-9">
+      <div
+        class="
+          max-w-7xl
+          mx-auto
+          sm:px-15
+          md:px-10
+          lg:px-15
+          xl:px-0
+          py-20
+          lg:flex lg:justify-around
+          gap-x-9
+        "
+      >
         <!-- <i class="icon-vector-1" /> -->
-        <img class="lg:mt-16" src="~/assets/images/home-about.svg"/>
+        <img class="lg:mt-16" src="~/assets/images/home-about.svg" />
         <div class="mt-16 lg:mt-0">
           <h3 class="uppercase font-bold text-[26px] tracking-[.125em] mb-7">
             About
@@ -12,13 +24,13 @@
           <hr class="line-heading mb-7" />
           <div class="lg:max-w-md">
             <div class="flex gap-x-10 mb-8">
-              <button class="uppercase font-bold text-lg leading-3 text-gray-79">
+              <button
+                class="uppercase font-bold text-lg leading-3 text-gray-79"
+              >
                 About
               </button>
               <div class="line-tab"></div>
-              <button class="uppercase font-bold text-lg leading-3">
-                How
-              </button>
+              <button class="uppercase font-bold text-lg leading-3">How</button>
               <div class="line-tab"></div>
               <button class="uppercase font-bold text-lg leading-3">
                 Next
@@ -26,11 +38,16 @@
             </div>
             <div class="text-lg font-bold leading-7 text-justify">
               <p>
-                TRINVI by Transvision is a blockchain powered initiative for the broadcasting industry and beyond.
+                TRINVI by Transvision is a blockchain powered initiative for the
+                broadcasting industry and beyond.
               </p>
               <br />
               <p>
-                We at TRINVI believe that digital creators, fans and viewers should be able to interact in order to build better content and further extend that content’s universe.  Our initiators aim to bridge the broadcasting industry with the new digital economy and invites everyone to take part!
+                We at TRINVI believe that digital creators, fans and viewers
+                should be able to interact in order to build better content and
+                further extend that content’s universe. Our initiators aim to
+                bridge the broadcasting industry with the new digital economy
+                and invites everyone to take part!
               </p>
             </div>
           </div>
@@ -40,24 +57,35 @@
     <!-- section 2 end -->
     <!-- section 6 start -->
     <section ref="section6" id="section6" class="gradient-pink-purple">
-      <div class="max-w-7xl mx-auto sm:px-15 md:px-10 lg:px-15 xl:px-0 pt-20 pb-8">
+      <div
+        class="max-w-7xl mx-auto sm:px-15 md:px-10 lg:px-15 xl:px-0 pt-20 pb-8"
+      >
         <div class="flex justify-between items-center mb-10">
           <h2 class="font-bold text-[1.625rem] text-white uppercase">News</h2>
           <nuxt-link to="/news">
-            <span class="text-base text-white underline font-medium">See More
+            <span class="text-base text-white underline font-medium"
+              >See More
               <i class="icon-angle-right text-white font-semibold" />
             </span>
           </nuxt-link>
         </div>
-        <div class="
-          flex
-          lg:justify-start
-          justify-center
-          items-start
-          flex-wrap
-          lg:flex-nowrap
-          mb-8">
-          <nuxt-img format="webp" quality="80" src="/images/news-thumbnail.png" class="lg:mr-20 mr-0" />
+        <div
+          class="
+            flex
+            lg:justify-start
+            justify-center
+            items-start
+            flex-wrap
+            lg:flex-nowrap
+            mb-8
+          "
+        >
+          <nuxt-img
+            format="webp"
+            quality="80"
+            src="/images/news-thumbnail.png"
+            class="lg:mr-20 mr-0"
+          />
           <div class="text-white lg:mr-20 mr-0 w-full">
             <span class="font-normal text-base font-white mb-5 inline-block">
               MARCH 10,2022
@@ -73,15 +101,18 @@
             </p>
           </div>
         </div>
-        <div class="
-          grid
-          lg:grid-cols-3
-          grid-cols-1
-          mb-8
-          lg:mr-20
-          mr-0
-          pr-9
-          border-t-2 border-white border-opacity-20">
+        <div
+          class="
+            grid
+            lg:grid-cols-3
+            grid-cols-1
+            mb-8
+            lg:mr-20
+            mr-0
+            pr-9
+            border-t-2 border-white border-opacity-20
+          "
+        >
           <div class="text-white max-w-[564] w-full py-8 my-2 pr-9">
             <span class="font-normal text-sm font-white mb-5 inline-block">
               MARCH 10,2022
@@ -90,14 +121,17 @@
               Bored Ape Yacht Club Creators Just Bought CryptoPunks and Meebits
             </h2>
           </div>
-          <div class="
-            text-white
-            max-w-[564]
-            w-full
-            py-8
-            my-2
-            border-l border-white border-opacity-20
-            px-4">
+          <div
+            class="
+              text-white
+              max-w-[564]
+              w-full
+              py-8
+              my-2
+              border-l border-white border-opacity-20
+              px-4
+            "
+          >
             <span class="font-normal text-sm font-white mb-5 inline-block">
               MARCH 10,2022
             </span>
@@ -105,14 +139,17 @@
               Bored Ape Yacht Club Creators Just Bought CryptoPunks and Meebits
             </h2>
           </div>
-          <div class="
-            text-white
-            max-w-[564]
-            w-full
-            py-8
-            my-2
-            border-l border-white border-opacity-20
-            pl-9">
+          <div
+            class="
+              text-white
+              max-w-[564]
+              w-full
+              py-8
+              my-2
+              border-l border-white border-opacity-20
+              pl-9
+            "
+          >
             <span class="font-normal text-sm font-white mb-5 inline-block">
               MARCH 10,2022
             </span>
@@ -126,25 +163,54 @@
         <div class="max-w-7xl mx-auto sm:px-15 md:px-10 lg:px-15 xl:px-0">
           <div class="py-8 flex justify-between items-center text-center">
             <div class="px-4">
-              <nuxt-img format="webp" quality="50" src="/images/TechnologyNFT.png" class="mix-blend-multiply mb-2" />
+              <nuxt-img
+                format="webp"
+                quality="50"
+                src="/images/TechnologyNFT.png"
+                class="mix-blend-multiply mb-2"
+              />
               <p class="font-bold text-black">Technology NFT</p>
             </div>
             <div class="px-4">
-              <nuxt-img format="webp" quality="50" src="/images/NFTAgency.png" class="mix-blend-multiply mb-2" />
+              <nuxt-img
+                format="webp"
+                quality="50"
+                src="/images/NFTAgency.png"
+                class="mix-blend-multiply mb-2"
+              />
               <p class="font-bold text-black">NFT Agency</p>
             </div>
             <div class="px-4">
-              <nuxt-img format="webp" quality="50" src="/images/NFTYHoldings.png" class="mix-blend-multiply mb-2" />
+              <nuxt-img
+                format="webp"
+                quality="50"
+                src="/images/NFTYHoldings.png"
+                class="mix-blend-multiply mb-2"
+              />
               <p class="font-bold text-black">NFTY Holdings</p>
             </div>
             <div class="px-4">
-              <nuxt-img format="webp" quality="50" src="/images/HonestNFT.png" class="mix-blend-multiply mb-2" />
+              <nuxt-img
+                format="webp"
+                quality="50"
+                src="/images/HonestNFT.png"
+                class="mix-blend-multiply mb-2"
+              />
               <p class="font-bold text-black">Honest NFT</p>
             </div>
             <div class="px-4">
-              <nuxt-img format="webp" quality="50" src="/images/EntourageGlobal.png" class="mix-blend-multiply mb-2" />
+              <nuxt-img
+                format="webp"
+                quality="50"
+                src="/images/EntourageGlobal.png"
+                class="mix-blend-multiply mb-2"
+              />
               <p class="font-bold text-black">Entourage Global</p>
             </div>
+          </div>
+          <!-- Sponsor Slider Component  -->
+          <div class="text-center">
+            <slider />
           </div>
         </div>
       </div>
@@ -153,7 +219,8 @@
     <!-- section 7 start -->
     <section class="bg-white">
       <div class="max-w-7xl mx-auto sm:px-15 md:px-10 lg:px-15 xl:px-0">
-        <div class="
+        <div
+          class="
             py-20
             grid grid-cols-1
             lg:grid-cols-2
@@ -161,7 +228,9 @@
             text-center
             lg:text-left
             justify-items-center
-            gap-10">
+            gap-10
+          "
+        >
           <h1
             class="
               font-bold
@@ -171,13 +240,17 @@
               from-[#FF5800]
               via-[#D02379] via-[#893C8E]
               to-[#023A59]
-              max-w-sm">
+              max-w-sm
+            "
+          >
             IS THIS YOU FIRST NFT?
           </h1>
           <div class="text-3xl max-w-[286px]">
             <p class="font-bold mb-[47px]">Learn How To:</p>
             <p class="text">Setup your wallet</p>
-            <p><span class="font-bold text-[#FF5800]">Purchase Etherium</span></p>
+            <p>
+              <span class="font-bold text-[#FF5800]">Purchase Etherium</span>
+            </p>
             <p>Be on a whitelist</p>
             <p>Mint an NFT</p>
           </div>
@@ -190,15 +263,17 @@
 <script lang="ts">
 import { defineComponent, ref } from '@nuxtjs/composition-api'
 import { useDaerah } from '@/compositions'
+import Slider from '~/components/Slider.vue'
 export default defineComponent({
+  components: { Slider },
   name: 'IndexPage',
-  setup () {
+  setup() {
     const section6 = ref(null)
     const { fetchProvince } = useDaerah()
     fetchProvince()
     return {
-      section6
+      section6,
     }
-  }
+  },
 })
 </script>
